fc9d96a8dfcc8571f3620b26430a19b55b4a0534
linduxed <linduxed@gmail.com>
2012-11-12 22:32:25 +0100

    Added seqDistance.
    Also added necessary comparison functions like sameLength and
    sameMolType.

--- MolSeq.hs
+++ MolSeq.hs
@@ -1,5 +1,6 @@
 module MolSeq (string2seq, seqDistance) where
 
+import Data.Function (on)
 
 data Molecule = DNA | Protein deriving (Show, Eq)
 data MolSeq = MolSeq
@@ -21,3 +22,23 @@ determineMolType inSequence
         aminoacids  = "ARNDCEQGHILKMFPSTWYV"
 
 seqDistance :: MolSeq -> MolSeq -> Float
+seqDistance a b
+    | not $ sameLength  a b                     = error "Sequences are not of same length."
+    | not $ sameMolType a b                     = error "Can't compare DNA to a Protein."
+    | molType a == DNA     && difference > 0.74 = 3.3
+    | molType a == Protein && difference > 0.94 = 3.7
+    | otherwise                                 = calcDistance
+    where
+        sequenceLength   = length $ molSequence a
+        differentLetters = length $ filter (uncurry (==)) $ (zip `on` molSequence) a b
+        difference       = ((/) `on` fromIntegral) differentLetters sequenceLength
+
+        calcDistance
+            | molType a == DNA = -3/4 * log (1 - 4/3 * difference)
+            | otherwise        = -19/20 * log (1 - 20/19 * difference)
+
+sameMolType :: MolSeq -> MolSeq -> Bool
+sameMolType = (==) `on` molType
+
+sameLength :: MolSeq -> MolSeq -> Bool
+sameLength = (==) `on` (length . molSequence)
