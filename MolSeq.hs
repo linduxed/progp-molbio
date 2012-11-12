4b560ba27cf14f0d2dc09a2fe7c08142e8b03598
linduxed <linduxed@gmail.com>
2012-11-12 22:31:09 +0100

    Added string2seq.

--- MolSeq.hs
+++ MolSeq.hs
@@ -9,5 +9,15 @@ data MolSeq = MolSeq
     } deriving (Show)
 
 string2seq :: String -> String -> MolSeq
+string2seq inName inSequence = MolSeq inName inSequence (determineMolType inSequence)
+
+determineMolType :: String -> Molecule
+determineMolType inSequence
+    | all (`elem` nucleotides) inSequence = DNA
+    | all (`elem` aminoacids)  inSequence = Protein
+    | otherwise                           = error "Sequence contains unknown letters."
+    where
+        nucleotides = "ACGT"
+        aminoacids  = "ARNDCEQGHILKMFPSTWYV"
 
 seqDistance :: MolSeq -> MolSeq -> Float
