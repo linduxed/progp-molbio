7995f14269401d9e487b9804b0221e80da031f2e
linduxed <linduxed@gmail.com>
2012-11-14 02:19:21 +0100

    Added fromMolSeqs function.
    generateMatrix is called but not yet written.

--- Profile.hs
+++ Profile.hs
@@ -11,3 +11,14 @@ data Profile = Profile
     , profType     :: ProfileType
     , profMatrix   :: ProfileMatrix
     } deriving (Show, Eq)
+
+fromMolSeqs :: [MolSeq] -> Profile
+fromMolSeqs sequences = Profile name sequenceCount seqType (generateMatrix sequences) where
+    name = show sequenceCount ++ "seq " ++ show seqType
+    sequenceCount = length sequences
+    seqType = checkAllSequencesType sequences
+
+    checkAllSequencesType seqList
+        | all ((== DNA) . molType)     seqList = DNAProf
+        | all ((== Protein) . molType) seqList = ProteinProf
+        | otherwise                            = error "Can't create profile for more than one type of sequence."
